0728ed3cd9f997a067a4b148e47b40397539a16a
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2022-10-05 00:25:07 +0530

    setup sass

---
+++ src/components/shared/seo/Seo.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Seo = () => {
+	return (
+		<Head>
+			<title>Sreekar Venkata Nutulapati</title>
+			<meta name='description' content='Full Stack Developer' />
+			<link rel='icon' href='/favicon.ico' />
+		</Head>
+	);
+};
+
+export default Seo;
