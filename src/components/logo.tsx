8b2fdc02d394149fb54ab7593c759d3d5f49ff05
Sreekar <sreekarnv1@gmail.com>
2023-03-15 06:24:48 +0530

    implement header - navbar, sidebar and add hi icons

---
+++ src/components/logo.tsx
@@ -0,0 +1,27 @@
+import type { Component } from 'solid-js';
+
+import '../scss/components/logo.scss';
+
+interface LogoProps {}
+
+const Logo: Component<LogoProps> = ({}) => {
+	return (
+		<>
+			<a class='logo' href='/'>
+				<img
+					class='logo__image'
+					src='/logo.webp'
+					height={60}
+					width={60}
+					alt='Sreekar Nutulapati'
+				/>
+				<div class='logo__content'>
+					<span class='logo__content__name'>Sreekar Nutulapati</span>
+					<span class='logo__content__designation'>Developer</span>
+				</div>
+			</a>
+		</>
+	);
+};
+
+export default Logo;
