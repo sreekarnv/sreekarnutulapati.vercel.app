1e8803b723697d1d2c1536dd1df1f93b154e1dab
Sreekar <sreekarnv1@gmail.com>
2023-03-16 21:33:01 +0530

    setup sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,24 @@ import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import vercel from '@astrojs/vercel/serverless';
 import image from '@astrojs/image';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sreekarnutulapati.vercel.app',
   integrations: [
     solidJs(),
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    sitemap({
+      changefreq: 'weekly',
+      customPages: [
+        'https://sreekarnutulapati.vercel.app',
+        'https://sreekarnutulapati.vercel.app/about',
+        'https://sreekarnutulapati.vercel.app/work',
+        'https://sreekarnutulapati.vercel.app/contact',
+      ],
     }),
   ],
   output: 'server',
